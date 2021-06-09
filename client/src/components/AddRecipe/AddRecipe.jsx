5f73f5be68d3cbb1337b783f3a0cba53c4fed05b
MJULIAN90 <michaeljulian50@gmail.com>
2021-06-08 23:11:32 -0500

    Client 3.0

--- client/src/components/AddRecipe/AddRecipe.jsx
+++ client/src/components/AddRecipe/AddRecipe.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function AddRecipe() {
   const [food, setfood] = useState({
@@ -9,8 +10,28 @@ function AddRecipe() {
     nivel_comida: "",
     pasos: "",
   });
-  function handleSumit(e) {
+  async function handleSumit(e) {
     e.preventDefault();
+
+    try {
+      await axios({
+        url: "http://localhost:3001/recipe",
+        method: "Post",
+        data: food,
+      });
+    } catch (err) {
+      alert("ERROR AL CREAR ");
+    }
+
+    alert(`PLATO ${food.nombre} HA SIDO CREADO`);
+
+    setfood({
+      nombre: "",
+      Resumen_plato: "",
+      Puntuacion: "",
+      nivel_comida: "",
+      pasos: "",
+    });
   }
 
   function handleOnchange(e) {
@@ -22,6 +43,7 @@ function AddRecipe() {
 
   return (
     <div>
+      <h2>AGREGAR PLATO</h2>
       <form onSubmit={handleSumit}>
         <div>
           <label>Nombre Del Plato: </label>
@@ -79,8 +101,14 @@ function AddRecipe() {
             onChange={handleOnchange}
           />
         </div>
+        <div>
+          <label>Dietas</label>
+        </div>
+        <button type="submit"> Crear</button>
       </form>
       <Link to="/Home">Volver </Link>
+
+      <button> Crear Nuevo Plato</button>
     </div>
   );
 }
