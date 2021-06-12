5d5671660d2dd770cfde1e43a5092dda76c291a4
MJULIAN90 <michaeljulian50@gmail.com>
2021-06-12 15:15:07 -0500

    Client 4.1

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -8,26 +8,63 @@ function Recipes() {
 
   const { recipes } = state;
 
+  let data = [...recipes];
+
+  //const [pagination, setpagination] = useState(0);
+
+  const nextPage = () => {};
+
+  const prevPage = () => {};
+
   return (
     <div>
-      {recipes.map((recipe) => (
-        <Link key={recipe.id} to={`/Home/Details/${recipe.id}`}>
+      {recipes.map((data) => (
+        <Link key={data.id} to={`/Home/Details/${data.id}`}>
           <div>
-            <h2>{recipe.title}</h2>
-            <img
-              src={recipe.image}
-              alt="error cargando"
-              width="160"
-              height="160"
-            />
-            <h3>
-              Diets: <br /> {recipe.diets}
-            </h3>
+            <h3>{data.title}</h3>
+            <img src={data.image} alt="error cargando" width="80" height="80" />
+            <h4>
+              Diets: <br /> {data.diets}
+            </h4>
           </div>
         </Link>
       ))}
+      <div>
+        <button type="button"> Anterior</button>
+        <button type="button"> 1</button>
+        <button type="button"> 2</button>
+        <button type="button"> 3</button>
+        <button type="button"> 4</button>
+        <button type="button"> 5</button>
+        <button type="button"> 6</button>
+        <button type="button"> 7</button>
+        <button type="button"> 8</button>
+        <button type="button"> 9</button>
+        <button type="button"> 10</button>
+        <button type="button"> Siguiente</button>
+      </div>
     </div>
   );
 }
 
 export default Recipes;
+
+/*      <button
+          type="button"
+          onClick={() => {
+            let uno = data.slice(0, 1);
+            console.log(uno);
+          }}
+        >
+          1
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            let dos = recipes.slice(1, 2);
+            console.log(dos);
+          }}
+        >
+          {" "}
+          2
+        </button> */
